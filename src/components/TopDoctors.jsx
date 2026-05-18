dd32263ed1e70d7c5dd4b3ff024544b0b5bb16ea
Nimmala Durga Mahendra <durgamahendranimmala@gmail.com>
2026-05-19 00:35:48 +0530

    Update TopDoctors.jsx

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -19,7 +19,7 @@ const TopDoctors = () => {
                             <div className={`flex items-center gap-2 text-sm text-center ${item.available ? 'text-green-500' : "text-gray-500"}`}>
                                 <p className={`w-2 h-2 rounded-full ${item.available ? 'bg-green-500' : "bg-gray-500"}`}></p><p>{item.available ? 'Available' : "Not Available"}</p>
                             </div>
-                            <p className='text-[#262626] text-lg font-medium'>{item.name}</p>
+                            <p className='text-[#262626] text-lg font-medium'>{item.name} hello</p>
                             <p className='text-[#5C5C5C] text-sm'>{item.speciality}</p>
                         </div>
                     </div>
